874d9e5cc0859dcccba8607c8a7ac87da82a1564
SanjuManna <sanjumanna2201@gmail.com>
2023-03-10 23:56:59 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,31 @@ import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 
 
-const getData = async () =>{
+const getData = async () => {
   const rawData = await fetch('https://jsonplaceholder.typicode.com/todos')
   const data = await rawData.json()
   return data
 }
 const Loader = () => <div id="loader">Loading...</div>
-const App = () => {
-  const [todos,setTodos] = useState([])
 
-  useEffect(()=>{
-    getData().then(data=> {
+
+const App = () => {
+  const [todos, setTodos] = useState([])
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getData().then(data => {
       setTodos(data)
-     })
-  },[])
+      setLoading(false)
+    })
+  }, [])
   return (
     <div id="main">
       {
         loading ? <Loader /> :
-      todos.map(todo=> <div id={`todo-${todo.id}`}>{todo.title}</div>)
+          todos.map(todo => <div key={todo.id} id={`todo-${todo.id}`}>{todo.title}</div>
+          )
       }
     </div>
   )
